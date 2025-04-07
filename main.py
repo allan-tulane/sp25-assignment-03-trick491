22b8f741fb2106a7cae4f597d24758bf926bd84a
trick491 <143121688+trick491@users.noreply.github.com>
2025-04-06 20:33:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,68 @@ alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev-
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
     if (S == ""):
-        return(len(T))
+        return (len(T))
     elif (T == ""):
-        return(len(S))
+        return (len(S))
     else:
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return (MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
+
+    if T == "":
+        return len(S)
+    if S == "":
+        return len(T)
+
+    elif S[0] == T[0]:
+        MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
+    else:
+        MED[(S, T)] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+
+    return MED[(S, T)]
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
-
+    if (S, T) in MED:
+        return MED[(S, T)]
+    
+    if S == "":
+        result = ("-" * len(T), T)
+        MED[(S, T)] = result
+        return result
+    elif T == "":
+        result = (S, "-" * len(S))
+        MED[(S, T)] = result
+        return result
+    
+    if S[0] == T[0]:
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        result = (S[0] + align_S, T[0] + align_T)
+        MED[(S, T)] = result
+        return result
+    
+    ins_cost = fast_MED(S, T[1:], {})  
+    del_cost = fast_MED(S[1:], T, {})    
+    sub_cost = fast_MED(S[1:], T[1:], {}) 
+    
+    ins_S, ins_T = fast_align_MED(S, T[1:], {})
+    del_S, del_T = fast_align_MED(S[1:], T, {})
+    sub_S, sub_T = fast_align_MED(S[1:], T[1:], {})
+    
+    min_cost = min(ins_cost, del_cost, sub_cost)
+    
+    
+    if ins_cost <= del_cost and ins_cost <= sub_cost:
+        result = ("-" + ins_S, T[0] + ins_T) 
+    elif del_cost <= sub_cost:
+        result = (S[0] + del_S, "-" + del_T) 
+    else:
+        result = (S[0] + sub_S, T[0] + sub_T) 
+    
+    MED[(S, T)] = result
+    return result
